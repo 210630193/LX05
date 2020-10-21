bf13869cf03f37117c64bfbac80d34610b29f74c
210630193 <65148108+210630193@users.noreply.github.com>
2020-10-21 15:39:02 +0800

    Add files via upload

---
+++ Activity.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Razor.Language.CodeGeneration;
+
+namespace DAL
+{
+    public class Activity
+    {
+        private Activity() { }
+        private static Activity _instance = new Activity();
+        public static Activity Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        string cns = AppConfigurtaionServices.Configuration.GetConnectionString("cns");
+        public Model.Activity GetModel(int id)
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "select activityId,activityName,endTime,activityPicture,summary,activityVerify,userName,recommend,recommendtime,activityIntroduction,endTime>=now() as activityStatus from activity where activityId=@id";
+                return cn.QueryFirstOrDefault<Model.Activity>(sql, new { id = id });
+            }
+        }
+    }
+}
+    public interface GetVerifyCount(){
+        using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "select count(1) from activity where activityVerify='审核通过'";
+                return cn.ExcuteScalar<int>(sql);
+        }
+
+
+    }
+    public IEnumberable<Model.Activity> GetVrifyPage(Model.Page page)
+{
+    using (IDbConnection cn = new MySqlConnection(cns))
+    {
+        string sql = "select* from  activity where activityVerify='审核通过' and endTime>=DesignTimeNodeWriter() order by endTime desc limit 2";
+
+        sql += "select* from a where num between (@pageIndex-1)*@pageSize+1 and @pageIndex*@pageSize;";
+        return cn.Query<Model.Activity>(sql, page);
+    }
+}
+public IEnumberable<Model.Activity> GetNew()//返回
+{
+    using (IDbConnection cn = new MySqlConnection(cns))
+    {
+       
+        string sql= "select* from activity where activityVerify='审核通过'and endTime>= now order by endTime desc limit 2";
+
+        return cn.Query<Model.Activity>(sql);
+    }
+}
+public Model.Activity GetRecommend()
+{
+    using (IDbConnection cn = new MySqlConnection(cns))
+    {
+
+        string sql = "select* from activity where activityVerify='审核通过'and recommend='是' and endTime>= now order by endTime desc limit 1";
+
+        return cn.QueryFirstOrDefault<Model.Activity>(sql);
+    }
+}
+
+
+//5
+
+public Model.Activity GetEnd()
+{
+    using (IDbConnection cn = new MySqlConnection(cns))
+    {
+
+        string sql = "select* from activity where activityVerify='审核通过'and recommend='是' and endTime>= now order by endTime desc limit 1";
+
+        return cn.QueryFirstOrDefault<Model.Activity>(sql);
+    }
+}public IEnumberable<Model.ActivityName> GetActivityNames()
+{
+    using (IDbConnection cn = new MySqlConnection(cns))
+    {
+        string sql = "select activityId,activityName from activity";
+        return cn.Query<Model.ActivityName>(sql);
+    }
+}
+public interface GetCount()
+    {
+    using (IDbConnection cn = new MySqlConnection(cns))
+    {
+        string sql = "select count(1) from activity";
+        return cn.ExecuteScalar<int>(sql);
+    }
+}
+public IEnumberable<Model.ActivityNo> GetPage(Model.Page page)
+    {
+    using (IDbConnection cn = new MySqlConnection(cns))
+_number() over(order by endTime desc)as num,";    {
+        string sql = "with a as(select row_number() over(order by endTime desc) as num, activity.* from activity)";
+
+        return cn.Query<Model.ActivityNo>(sql,page);
+    }
+}
+//6
+public interface Add(Model.Activity active)
+    {
+    using (IDbConnection cn = new MysqlConnection(cns))
+    {
+        string sql = "insert into activity(activityname,endtime,activitypicture,activityintroduction,summary,activityverify,activitystatus,username,recommend) values(@activityName, @endTime, @activityPicture, @activityIntroduction, @summary, @activityVerify, @activityStatus, @userName, @recommend);";
+        return cn.ExecuteScalar<int>(sql, active);
+
+    }
+}
+public interface Update(Model.Activity active)
+    {
+    using (IDbConnection cn = new MysqlConnection(cns))
+        string sql = "update activity set activityname=@activityName, endtime=@endTime, activitypicture=@activityPicture, activityintroduction=@activityIntroduction, summary=@summary, activityverify=@activityVerify, activitystatus=@activityStatus where activityid=@activityId";
+    return cn.Excute(Sql, active);
+}
+}
+public interface UpdateImg(Model.Activity activity)
+    {
+    using (IDbConnection cn = new MysqlConnection(cns))
+    {
+        string sql = "uadate activity set activitypicture=@activityPicture where activityid=@activityid";
+        return cn.Execute(Sql, active);
+    }
+}
+//7
+public interface Delete(int id)
+    {
+    using (IDbConnection cn = new MysqlConnection(cns))
+    {
+        string sql = "delete  from activity where activityid=@id";
+        return cn.Execute(sql, new { id = id });
+    }
+}
+public interface UpdateRecommend(Model.Activity activity)
+    {
+    using (IDbConnection cn = new MysqlConnection(cns))
+    {
+        string sql = "uadate activity set recommend=@recommend,recommendTime=@recommendTime where activityid=@activityId";
+        return cn.Execute(sql, activity);
+    }
+}
---
+++ ActivityController.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityController : ControllerBase
+    {
+        public readonly IHostingEnvironment _hostingEnvironment;
+        public ActivityController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return JsonResult(DAL.Activity.Instance.GetCount());
+        }
+        [HttpGet("verifyCount")]
+        public ActionResult GetVerifyCount()
+        {
+            return JsonResult(DAL.Activity.Instance.GetVerifyCount());
+        }
+    }
+    //10
+    [HttpGet("recommend")]
+    public ActionResult GetRecommend()
+    {
+        var result = DAL.Activity.Instance.GetRecommend();
+        if (result != null)
+            return JsonResult(Result.Ok(result));
+        else
+            return JsonResult(Result.Err("记录数为0"));
+    }
+    //10
+    [HttpGet("end")]
+    public ActionResult GetEnd()
+    {
+        var result = DAL.Activity.Instance.GetEnd();
+        if (result != null)
+            return Json(Result.Ok(result));
+        else
+            return Json(Result.Err("记录数为0"));
+    }
+    [HttpGet("names")]
+    public ActionResult GetNames()
+    {
+        var result = DAL.Activity.Instance.GetActivityNames();
+        if (result.Count() == 0)
+            return Json(Result.Err("没有任何活动"));
+        else
+            return Json(Result.Ok(result));
+    }
+    //12
+    [HttpGet("id")]
+    public ActionResult Get(int id)
+    {
+        var result = DAL.Activity.Instance.GetModel(id);
+        if (result != null)
+            return Json(Result.Ok(result));
+        else
+            return Json(Result.Err("activityID不存在"));
+    }
+    [HttpPost]
+    public ActionResult Post([FromBody] Model.Activity activity)
+    {
+        activity.activityIntroduction = activity.activityIntroduction.Replace($"https://{HttpContext.Request.Host.Value}/", "");
+        activity.recommend = "否";
+        try
+        {
+            int n = DAL.Activity.Instance.Add(activity)
+                return Json(Result.Ok("发布活动成功", n));
+        }
+        catch (Exception ex)
+        {
+            if (ex.Message.ToLower().Contains("foreign key"))
+                return Json(Result.Err("合法用户才能添加记录"));
+            else if (ex.Message.ToLower().Contains("null"))
+                return Json(Result.Err("活动名称、...不能为空"));
+            else
+                return Json(Result.Err(ex.Message));
+        }
+        //16
+        [HttpPut]
+        public ActionResult Put([FromBody] Model.Activity activity)
+        {
+            activity.activityIntroduction = activity.activityIntroduction.Replace($"https://{HttpContext.Request.Host.Value}/", "");
+            try
+            {
+                var n = DAL.Activity.Instance.Update(activity);
+                if (n != 0)
+                    return Json(Result.Ok("修改活动成功", activity.activityId));
+                else
+                    return Json(Result.Err("activityID不存在"));
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.ToLower().Contains("null")) ;
+                return Json(Result.Err("活动名称、...不能为空"));
+                else
+                    return Json(Result.Err(ex.Message));
+            }
+        }
+        [HttpDelete("{id}")]
+        Public ActionResult  Delete(int id)
+            {
+            try
+            {
+                var n = DAL.Activity.Instance.Delete(id);
+                if (n = != 0)
+                    return Jsonn(Result.Ok("删除成功"));
+                else
+                    return Json(Result.Err("activityID不存在"));
+            }
+            catch (Exception ex)
+            {
+                return Json(Result.Err(ex.Message));
+            }
+        }
+        //18
+        [HttpPost("page")]
+        Public ActionResult getPage([FromBody] Model.Page page)
+            {
+            var result = DAL.Activity.Instance.Getpage(page);
+            if (result.Count() == 0)
+                result Json(Result.Err("返回记录数为0"));
+            else
+                return Json(Result.Ok(result));
+        }
+        [HttpPost("verifyPage")]
+        Public ActionResult getVerifyPage([FromBody] Model.Page page)
+            {
+            var result = DAL.Activity.Instance.GetVerifyPage(page);
+            if (result.Count() == 0)
+                result Json(Result.Err("返回记录数为0"));
+            else
+                return Json(Result.Ok(result));
+        }
+        //20
+        [HttpPut("Verify"]
+        Public ActionResult PutVerify([FromBody]Model.Activity activity)
+             {
+            var n = DAL.Activity.Instance.UpdateVerify(activity);
+            if (n = != 0)
+                return Jsonn(Result.Ok("审核活动成功", activity.activityId));
+            else
+                return Json(Result.Err("activityID不存在"));
+        }
+            catch (Exception ex)
+        {
+            (ex.Message.ToLower().Contains("null"));
+            return Json(Result.Err("活动名称、...不能为空"));
+                else
+                return Json(Result.Err(ex.Message));
+        }
+    }
+        [HttpPut("Recommend")]
+    Public ActionResult PutRecommend([FromBody]Model.Activity activity)
+    {
+        activity.recommendTime = DateTime.Now;
+        try
+        {
+            var re = "";
+            if (activity.recommend == "否") re = "取消";
+            var n = DAL.Activity.Instance.UpdateRecommend(activity);
+            if (n = != 0)
+                return Jsonn(Result.Ok("审核活动成功", activity.activityId));
+            else
+                return Json(Result.Err("activityID不存在"));
+        }
+        catch (Exception ex)
+        {
+            (ex.Message.ToLower().Contains("null"));
+            return Json(Result.Err("推荐活动情况不能为空"));
+                else
+                return Json(Result.Err(ex.Message));
+        }
+    }
+    //22
+    [HttpPut("{id}"]
+    Public ActionResult upImg(int id, List<IFormFile> files)
+    {
+        var path = System.IO.Path.Combine(_hostingEnvironment.WebRootPath, "img", "Activity");
+        var fileName = $"{path}/{id}";
+        try
+        {
+            var ext = DAL.Activity.Instance.UpImg(files[0], fileName);
+            if(ext == null)
+                return Json(Result.Err("请上传图片文件"));
+            else
+            {
+                var file = $"img/Activity/{id}{ext}";
+                Model.Activity activity = new Model.Activity() { activityId = id, activityPicture = file };
+                var n = DAL.Activity.Instance.UpdateImg(activity);
+                if (n > 0)
+                    return Jsonn(Result.Ok("上传成功", file));
+                else
+                    return Json(Result.Err("请输入正确的活动id"));
+            }
+        }
+        catch (Exception ex)
+        {
+            return Json(Result.Err(ex.Message));
+        }
+    }
+
